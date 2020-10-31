6520dc43601345dcb61e01c0d554e729be5a28c7
slushercj <slusher.cj@gmail.com>
2020-10-31 13:14:19 -0700

    Remove Tabs

--- App.js
+++ App.js
@@ -2,28 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
 import MapScreen from './src/screens/MapScreen';
-import MapListScreen from './src/screens/MapListScreen';
 import { setNavigator } from "./src/navigationRef";
 
-const switchNavigator = createSwitchNavigator({
-  mainFlow: createBottomTabNavigator({
-    MapView: MapScreen,
-    MapList: MapListScreen,
-  })
-})
-
-const App = createAppContainer(switchNavigator)
-
-export default () => {
+const AppsScreen = () => {
   return (
-    <App
-      ref={navigator => {
-        setNavigator(navigator)
-      }}
-    />
-  );
+    <View>
+      <MapScreen />
+    </View>
+  )
 }
 
+export default AppsScreen;
 
